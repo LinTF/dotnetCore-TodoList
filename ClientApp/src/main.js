f7c4baf8297595fd56ce7fdc39d253122b72f142
Iris <lin.tf1124@gmail.com>
2023-12-26 17:17:33 +0800

    axios安裝與引用

--- ClientApp/src/main.js
+++ ClientApp/src/main.js
@@ -4,5 +4,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap"
 import '@fortawesome/fontawesome-free/js/all.js';
 import "../src/assets/scss/style.scss"
+import axios from "./axios/axios.js"
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.mount('#app');
+
+app.config.globalProperties.$axios = axios;
